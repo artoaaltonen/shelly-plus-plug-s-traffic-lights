da559e5981ee5fa8fcfd41881aea85cce137a9bd
Arto Aaltonen <49676133+artoaaltonen@users.noreply.github.com>
2023-05-04 21:38:46 +0300

    Update default schedule

--- TrafficLights.js
+++ TrafficLights.js
@@ -1,7 +1,7 @@
 // https://github.com/artoaaltonen/shelly-plus-plug-s-traffic-lights
 
-let greenStart = 2134; // 06:45
-let greenEnd = 2136; // 09:00
+let greenStart = 0645; // 06:45
+let greenEnd = 0900; // 09:00
 
 let configGreen = {"leds":{"mode":"switch","colors":{"switch:0":{"on":{"rgb":[0,100,0],"brightness":25},"off":{"rgb":[0,100,0],"brightness":25}},"power":{"brightness":100}},"night_mode":{"enable":false,"brightness":0,"active_between":["09:00","07:00"]}},"controls":{"switch:0":{"in_mode":"momentary"}}};
 let configRed = {"leds":{"mode":"switch","colors":{"switch:0":{"on":{"rgb":[100,0,0],"brightness":15},"off":{"rgb":[100,0,0],"brightness":15}},"power":{"brightness":100}},"night_mode":{"enable":false,"brightness":0,"active_between":["09:00","07:00"]}},"controls":{"switch:0":{"in_mode":"momentary"}}};
